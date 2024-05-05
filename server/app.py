d7f2f85892410e3a258aa0a1375c138bd29fa053
alexsylvian <lazeratus1@gmail.com>
2024-05-05 18:24:16 -0400

    serialize

--- server/app.py
+++ server/app.py
@@ -16,11 +16,40 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
-    pass
+# class Plants(Resource):
+#     pass
 
-class PlantByID(Resource):
-    pass
+# class PlantByID(Resource):
+#     pass
+
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.serialize() for plant in plants])
+
+# Show Route
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get(id)
+    if plant:
+        return jsonify(plant.serialize())
+    else:
+        return jsonify({'message': 'Plant not found'}), 404
+
+# Create Route
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    name = data.get('name')
+    image = data.get('image')
+    price = data.get('price')
+    new_plant = Plant(name=name, image=image, price=price)
+    db.session.add(new_plant)
+    db.session.commit()
+    return jsonify(new_plant.serialize()), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
         
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    image = db.Column(db.String(200))
+    price = db.Column(db.Float(precision=2))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price
+        }
